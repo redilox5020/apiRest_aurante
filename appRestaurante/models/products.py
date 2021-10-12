02f1438298d2fb45df5c596e9594a70d4ca6fd97
redilox5020 <73054547+redilox5020@users.noreply.github.com>
2021-10-12 11:00:18 -0500

    Update products.py

--- appRestaurante/models/products.py
+++ appRestaurante/models/products.py
@@ -1,9 +1,14 @@
 from django.db import models
 
 class Product(models.Model):
-    id                = models.AutoField(primary_key=True)
-    nombre            = models.CharField(max_length = 50)
-    precio            = models.IntegerField(default = 0 )
-    tipo              = models.CharField(max_length = 50)
-    marca             = models.CharField(max_length = 50, null= True)
-    ingredientes      = models.CharField(max_length = 255, null=True)
+    id              = models.AutoField(primary_key=True)
+    name            = models.CharField(max_length = 50)
+    price           = models.IntegerField(default = 0 )
+    type            = models.CharField(max_length = 50)
+    make            = models.CharField(max_length = 50, null= True)
+    Description     = models.TextField()
+    stock           = models.IntegerField(default=1)
+    thumbnail       = models.ImageField()
+    slug            = models.SlugField() # identificador por nombre del Producto en url
+    publish_date    = models.DateTimeField(auto_now_add=True)
+    last_updated    = models.DateTimeField(auto_now=True)
